1a5c52c6dbf4a5ac2998a615ac28ceeab9b8b00f
ClementeEyezen <mobile.wbaskin@gmail.com>
2015-08-05 12:53:32 -0400

    Added new template + view rendering

--- web_app/app/views.py
+++ web_app/app/views.py
@@ -1,7 +1,11 @@
 from web_app.app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 
 def index():
-    return "Hello, World!"
+    user = {'nickname': 'Buck'}  # fake user
+    return render_template('index.html',
+                           title='Home',
+                           user=user)
