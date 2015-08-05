1bb33863972c56f63fa147df72b2f2f501bdf540
ClementeEyezen <mobile.wbaskin@gmail.com>
2015-08-05 13:09:18 -0400

    Added extra server config, non-shared config file

--- web_app/config/server_config.py
+++ web_app/config/server_config.py
@@ -1,3 +1,7 @@
+from web_app.config import secret_config
+
 config = dict([('DEBUG', True), 
                ('PORT', 5000), 
-               ('HOST', '127.0.0.1')])
+               ('HOST', '127.0.0.1'),
+               ('WTF_CSRF_ENABLED', True),
+               ('SECRET_KEY', secret_config.SECRET_KEY)])
