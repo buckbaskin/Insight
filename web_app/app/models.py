1fc8d56beb0663a753f016a04e172da4a0530871
ClementeEyezen <mobile.wbaskin@gmail.com>
2015-08-05 23:45:25 -0400

    Added extra info to user

--- web_app/app/models.py
+++ web_app/app/models.py
@@ -2,16 +2,27 @@ from web_app.app import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    t_username = db.Column(db.String(120), index=True, unique=True)
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    t_screen_name = db.Column(db.String(120), index=True, unique=True)
+    
+    contributors = db.Column(db.Boolean)
+    created_at = db.Column(db.DateTime(True))
+    description = db.Column(db.String(400))
+    favourites_count = db.Column(db.Integer)
+    followers_count = db.Column(db.Integer)
+    following = db.Column(db.Boolean)
+    friends_count = db.Column(db.Integer)
+    t_id = db.Column(db.Integer)
+    name = db.Column(db.String(30))
+    statuses_count = db.Column(db.Integer)
+    verified = db.Column(db.Boolean)
 
     def __repr__(self):
         return '<User %r>' % (self.nickname)
     
 class Follows(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    follower = db.relationship('User', backref='follower', lazy='dynamic')
-    followee = db.relationship('User', backref='followee', lazy='dynamic')
+    follower = db.Column(db.Integer, db.ForeignKey('user.t_userame'), index=True)
+    followee = db.Column(db.Integer, db.ForeignKey('user.t_userame'), index=True)
     
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key = True)
