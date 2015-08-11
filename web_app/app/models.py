fd06cc0483213ca4a0fdb5726d9317348665ccf7
ClementeEyezen <mobile.wbaskin@gmail.com>
2015-08-11 13:20:25 -0400

    Revert "Moving model files into separate folder" as part of revert reorg
    This reverts commit 849af4aa790d24b953a9f84ac25d121ee56dcdd6.

--- web_app/app/models.py
+++ web_app/app/models.py
@@ -1,9 +1,42 @@
 from web_app.app import db
-from web_app.app.models.user_model import User
-from web_app.app.models.twitter_model import Follows, Status, Hashtag, Tags
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    t_screen_name = db.Column(db.String(120), index=True, unique=True)
+    
+    contributors = db.Column(db.Boolean)
+    created_at = db.Column(db.DateTime(True))
+    description = db.Column(db.String(400))
+    favourites_count = db.Column(db.Integer)
+    followers_count = db.Column(db.Integer)
+    following = db.Column(db.Boolean)
+    friends_count = db.Column(db.Integer)
+    t_id = db.Column(db.Integer)
+    name = db.Column(db.String(30))
+    statuses_count = db.Column(db.Integer)
+    verified = db.Column(db.Boolean)
+
+    def __repr__(self):
+        return '<User %r>' % (self.nickname)
+    
+    @staticmethod
+    def make_unique_nickname(nickname):
+        if User.query.filter_by(nickname=nickname).first() is None:
+            return nickname
+        version = 2
+        while True:
+            new_nickname = nickname + str(version)
+            if User.query.filter_by(nickname=nickname).first() is None:
+                break
+            version += 1
+        return new_nickname
+    
+class Follows(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    follower = db.Column(db.Integer, db.ForeignKey('user.t_userame'), index=True)
+    followee = db.Column(db.Integer, db.ForeignKey('user.t_userame'), index=True)
     
 class Post(db.Model):
-    # This is a post from the tutorial
     id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String(80))
     body = db.Column(db.String(140))
@@ -12,3 +45,16 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post %r>' % (self.body)
+    
+class Status(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    
+    favorited = db.Column(db.Boolean)
+    created_at = db.Column(db.DateTime)
+    
+class Tags(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    status = db.Column(db.Integer, db.ForeignKey(''))
+    
+class Hashtag(db.Model):
+    text = db.Column(db.String, primary_key = True)
