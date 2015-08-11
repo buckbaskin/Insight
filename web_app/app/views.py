839f328edc29738d18e45dba66c06101269b9b69
ClementeEyezen <mobile.wbaskin@gmail.com>
2015-08-11 13:06:53 -0400

    Moving routing to routes folder, out of 1 views file

--- web_app/app/views.py
+++ web_app/app/views.py
@@ -1,40 +1,21 @@
 from web_app.app import app
-from flask import render_template, flash, redirect
-from web_app.app.forms import LoginForm
+from flask import render_template
 from web_app.app import db
+from web_app.app.routes.core_routes import core_index
+from web_app.app.routes.user_routes import user_login
 
 @app.route('/')
 @app.route('/index')
 
 def index():
-    user = {'nickname': 'Buck'}  # fake user
-    posts = [  # fake array of posts
-        { 
-            'author': {'nickname': 'John'}, 
-            'body': 'Beautiful day in Portland!' 
-        },
-        { 
-            'author': {'nickname': 'Susan'}, 
-            'body': 'The Avengers movie was so cool!' 
-        }
-    ]
-    return render_template('index.html',
-                           title='Home',
-                           user=user, 
-                           posts=posts)
+    core_index()
     
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for OpenID="%s", remember_me=%s' %
-              (form.openid.data, str(form.remember_me.data)))
-        return redirect('/index')
-    return render_template('login.html', 
-                           title='Sign In',
-                           form=form,
-                           providers=app.config['OPENID_PROVIDERS'])
-    
+    user_login()
+
+# Default error handling
+
 @app.errorhandler(404)
 def not_found_error(error):
     return render_template('404.html'), 404
