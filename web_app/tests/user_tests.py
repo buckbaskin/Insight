f610a18f2627867835f7e772161ba6f66bd676a6
ClementeEyezen <mobile.wbaskin@gmail.com>
2015-08-24 17:14:34 -0400

    Added follow/follower testing

--- web_app/tests/user_tests.py
+++ web_app/tests/user_tests.py
@@ -23,5 +23,29 @@ class TestCase(unittest.TestCase):
         expected = 'http://www.gravatar.com/avatar/9a0c75a27f67d0496095d060f28fb8ed'
         assert avatar[0:len(expected)] == expected
         
+    def test_follow(self):
+        u1 = User('john')
+        u2 = User('susan')
+        db.session.add(u1)
+        db.session.add(u2)
+        db.session.commit()
+        assert u1.unfollow(u2) is None
+        u = u1.follow(u2)
+        db.session.add(u)
+        db.session.commit()
+        assert u1.follow(u2) is None
+        assert u1.is_following(u2)
+        assert u1.followed.count() == 1
+        assert u1.followed.first().t_screen_name == 'susan'
+        assert u2.followers.count() == 1
+        assert u2.followers.first().t_screen_name == 'john'
+        u = u1.unfollow(u2)
+        assert u is not None
+        db.session.add(u)
+        db.session.commit()
+        assert not u1.is_following(u2)
+        assert u1.followed.count() == 0
+        assert u2.followers.count() == 0
+        
 if __name__ == '__main__':
     unittest.main()
