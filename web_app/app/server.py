063c9621ef7ecee2f88ad1fa59bbb1378a27972b
Buck Baskin <mobile.wbaskin@gmail.com>
2015-08-29 08:08:50 +0000

    Changes that make the server work on RPi

--- web_app/app/server.py
+++ web_app/app/server.py
@@ -1,7 +1,15 @@
+#!/usr/bin/env python
+import sys
+# print 'sys_path:'
+# print sys.path
+# sys.path[0] = '/home/pi/Insight'
+sys.path.append('/home/pi/Insight')
+# print 'sys_path 2:'
+# print sys.path
 from web_app.app import app
 from web_app.config import server_config
 
 # === APP ===
 
 app.config.from_object(server_config)
-app.run()
+app.run(host='0.0.0.0', port=80)
