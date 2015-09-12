ace708a83865e6c75091865c93acf6a3ed3fd17e
ClementeEyezen <mobile.wbaskin@gmail.com>
2015-09-12 16:41:01 -0400

    WIP | Set AngularJS to poll db for data

--- web_app/app/static/main.js
+++ web_app/app/static/main.js
@@ -3,7 +3,7 @@
   'use strict';
   angular.module('Insight', [])
 
-  .controller('InsightController', ['$scope', '$log', '$http', function($scope, $log, $http) {
+  .controller('InsightController', ['$scope', '$log', '$http', '$timeout', function($scope, $log, $http, $timeout) {
     $scope.getResults = function() {
       $log.log("test");
       
@@ -11,13 +11,37 @@
 	  var userInput = $scope.input_url;
 	  // fire the API request
 	  $http.post('/start', {"url": userInput}).
-	    success(function(results) {
-	      $log.log(results);
-	    }).
-	    error(function(error) {
-	      $log.log(error);
-	    });
+		  success(function(results) {
+		    $log.log(results);
+		    getWordCount(results);
+		
+		  }).
+		  error(function(error) {
+		    $log.log(error);
+		  });
     };
+    
+    function getWordCount(jobID) {
+	  var timeout = "";
+	
+	  var poller = function() {
+	    // fire another request
+	    $http.get('/results/'+jobID).
+	      success(function(data, status, headers, config) {
+	        if(status === 202) {
+	          $log.log(data, status);
+	        } else if (status === 200){
+	          $log.log(data);
+	          $timeout.cancel(timeout);
+	          return false;
+	        }
+	        // continue to call the poller() function every 2 seconds
+	        // until the timeout is cancelled
+	        timeout = $timeout(poller, 2000);
+	      });
+	  };
+	  poller();
+}
   }
 
   ]);
