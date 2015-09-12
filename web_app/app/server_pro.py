f09d41a1a1e3adf6f862a353341a1cce859aa173
ClementeEyezen <mobile.wbaskin@gmail.com>
2015-09-12 17:31:41 -0400

    Remove everything and start again

--- web_app/app/server_pro.py
+++
@@ -1,7 +0,0 @@
-from web_app.app import app
-from web_app.config import server_config
-
-# === APP ===
-
-app.config.from_object(server_config)
-app.run(debug=False)
--- web_app/scripts/db_upgrade.py
+++
@@ -1,6 +0,0 @@
-from migrate.versioning import api
-from web_app.config.server_config import SQLALCHEMY_DATABASE_URI
-from web_app.config.server_config import SQLALCHEMY_MIGRATE_REPO
-api.upgrade(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
-v = api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
-print('Current database version: ' + str(v))
