9c1c199df85c896c69c863b3b04f331500606570
Buck Baskin <mobile.wbaskin@gmail.com>
2016-06-04 05:13:35 -0400

    Tutorial part 2 wip

--- app/views.py
+++ app/views.py
@@ -1,3 +1,5 @@
+from flask import render_template
+
 from app import server
 
 
@@ -5,9 +7,20 @@ from app import server
 @server.route('/index')
 def index():
     user = {'nickname': 'Buck'}  # fake user
+    posts = [
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Portland!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'Avengers!'
+        }
+    ]
     return render_template('index.html',
-       title='Home',
-                           user=user)
+                           title='Home',
+                           user=user,
+                           posts=posts)
 
 
 @server.route('/fast')
