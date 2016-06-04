06c877198b7c42e78ca1ce2cb2492279e2a11ec6
Buck Baskin <mobile.wbaskin@gmail.com>
2016-06-04 04:50:40 -0400

    Following tutorial part 2 - rescue

--- app/views.py
+++ app/views.py
@@ -1,11 +1,15 @@
 from app import server
 
+
 @server.route('/')
 @server.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'nickname': 'Buck'}  # fake user
+    return render_template('index.html',
+       title='Home',
+                           user=user)
+
 
 @server.route('/fast')
 def fast():
     return "f"
-
