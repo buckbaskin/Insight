79cdd12adaa15f3683bc5102421110d8c23836c7
Buck Baskin <buckbaskin@google.com>
2016-06-13 02:22:46 -0400

    Add rate limiting and request shaping to the load test. See #29

--- test/load_test.py
+++ test/load_test.py
@@ -2,21 +2,56 @@
 import grequests
 import itertools
 from multiprocessing import Process, Queue
+import numpy
 import queue
+import random
+import time
 
-how_many = 1000
+# qps = queries per second
+# qps > 0 - queries per second (uniform)
+# qps < 0 - approx. queries per second (random)
+# qps = 0 - no rate limit
+total_qps = -10
+how_many = 999
+how_many_processes = 1
+how_many_per_process = how_many // how_many_processes
+qps = abs(float(total_qps)/how_many_processes)
+del how_many
+target_url = 'http://127.0.0.1:5000/index'
 
-def run(queue, how_many):
+def uniform_yielder(iterable, rate):
+    delay = 1.0/rate
+    for item in iterable:
+        yield item
+        time.sleep(delay)
+
+def fast_yielder(iterable, rate):
+    for item in iterable:
+        yield item
+
+my_rand = random.Random(x='12345')
+def random_yielder(iterable, rate):
+    mean = 1.0/rate
+    stdev = 0.1/rate
+    delays = numpy.random.normal(mean, stdev, size=how_many_per_process)
+    delays_index = 0
+    for item in iterable:
+        yield item
+        time.sleep(delays[delays_index])
+        delays_index += 1
+
+def run(queue, how_many, time_generator, rate):
     success = 0
-    urls = itertools.repeat(object=('GET', 'http://127.0.0.1:5000/index',), times=how_many)
+    urls = itertools.repeat(object=('GET', target_url,), times=how_many_per_process)
     requests = itertools.starmap(grequests.AsyncRequest, urls)
-    responses = grequests.imap(requests=requests, size=how_many)
+    timed_requests = time_generator(requests, rate)
+    responses = grequests.imap(requests=timed_requests, size=how_many_per_process)
     
     for gg in responses:
         if (gg.status_code == 200):
             success += 1
 
-    queue.put((success, how_many,))
+    queue.put((success, how_many_per_process,))
     print('Donezo')
 
 def call_start(iterable):
@@ -31,7 +66,14 @@ def call_join(iterable):
 
 if __name__ == '__main__':
     q = Queue()
-    process_args = itertools.repeat(object=(None, run, None, (q, how_many,),), times=10)
+    if total_qps == 0:
+        time_generator = fast_yielder
+    elif total_qps < 0:
+        time_generator = random_yielder
+    else:
+        time_generator = uniform_yielder
+    process_args = itertools.repeat(object=(None, run, None, (q, how_many_per_process, time_generator, qps,),), times=how_many_processes)
+
     processes = itertools.starmap(Process, process_args)
     startup = call_start(processes)
 #    endup = call_join(startup)
