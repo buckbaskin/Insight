6da681cdd35f1c95caa6dd8cc4fa5572c42e1755
Buck Baskin <buckbaskin@google.com>
2016-06-14 02:08:36 -0400

    Test now returns non-zero when tests fail

--- runtests.sh
+++ runtests.sh
@@ -2,5 +2,11 @@ nosetests \
 --with-timer --timer-top-n 5 --timer-ok 250ms --timer-warning 500ms \
 --with-coverage --cover-erase --cover-package=Insight --cover-html --cover-branches
 # --cover-min-percentage=90
-echo $?
+result=$?
 echo "View coverage report @ file:///home/buck/Github/Insight/cover/index.html"
+if [ $result != 0 ]; then
+    echo $result
+    exit 1
+else
+    exit 0
+fi
