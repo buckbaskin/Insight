25f51f4cc44959d37a09aa7d65c8b9c53584ca2b
Buck Baskin <buckbaskin@google.com>
2016-06-18 14:15:12 -0400

    Queue manager can create workers but not kill them
    I'm going to need to rewrite my own worker that does a "work once"
    then checks a list to see if it's been killed, and loops. In this
    way, there will be one manager-worker that reads from a worker-
    create queue, and all the others read from a cycling worker-
    destroy queue or something like that.

--- sql/manager.py
+++ sql/manager.py
@@ -1,18 +1,46 @@
+import redis
+import rq
 import subprocess
 
 from rq import Worker
 
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
+
 if __name__ == '__main__':
+    open_process_list = {}
     while(True):
         selector = input('new or kill <id>? ')
-        if selector == 'new':
+        if selector == '':
+            pass
+        elif selector == 'new':
             args = ['rq', 'worker', 'userEvent', 'low']
-            subprocess.Popen(args)
-        elif selector[:4] == 'new ':
-# create with specific queues
-            pass
-        elif selector[:5] == 'kill ':
+            p = subprocess.Popen(args)
+            open_process_list[p.pid] = p
+            print(open_process_list)
+        elif len(selector) > 4 and selector[:4] == 'new ':
+            queues = selector.split(' ')
+            queues = queues[1:]
+            if len(queues) >= 1:
+                args = ['rq', 'worker']
+                args.extend(queues)
+                p = subprocess.Popen(args)
+                open_process_list[p.pid] = p
+                print(open_process_list)
+        elif len(selector) > 5 and selector[:5] == 'kill ':
 # kill by id
-            pass
+            worker_id = selector[5:]
+            worker = Worker.find_by_key(worker_id, connection=r)
+            if worker is None:
+                continue
+            else:
+                print('found kill worker: '+str(worker.key)+' with pid '+str(worker.pid))
+            print(open_process_list)
+            open_process_list[worker.pid].terminate()
+            del open_process_list[worker.pid]
         else:
             break
+        to_print = 'w { '
+        for worker in Worker.all(connection=r):
+            to_print += str(worker.key)+','
+        to_print += ' }'
+        print(to_print)
