a7217e350db46ab01f15741987e1ebbf0c8fbec7
Buck Baskin <buckbaskin@google.com>
2016-08-31 16:25:30 -0400

    Fix imports in automock/__init__

--- flaskserver/automock/__init__.py
+++ flaskserver/automock/__init__.py
@@ -1,2 +1,2 @@
-from Insight.automock import decorator as decorators
-from Insight.automock.mock_modules import mock_requests, mock_time, jsmock
+from automock import decorator as decorators
+from automock.mock_modules import mock_requests, mock_time, jsmock
