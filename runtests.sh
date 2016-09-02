a78f3d99ea3ff4b9328ee4622b5a9f6cde7e14ac
Buck Baskin <buckbaskin@google.com>
2016-09-02 16:11:47 -0400

    This should fix runtests, and pass (for lack of some testing)

--- runtests.sh
+++ runtests.sh
@@ -2,7 +2,8 @@ export STATUS=TESTING
 echo $STATUS
 nosetests \
 --with-timer --timer-top-n 5 --timer-ok 250ms --timer-warning 500ms \
---with-coverage --cover-erase --cover-package=Insight --cover-html --cover-branches
+--with-coverage --cover-erase --cover-html --cover-branches \
+flaskserver/tests/ service1/tests/
 # --cover-min-percentage=90
 result=$?
 echo "View coverage report @ file:///home/buck/Github/Insight/cover/index.html"
